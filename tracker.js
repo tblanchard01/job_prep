6144df1ff6de3ac1cc38d19a006189675b11f9a5
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-10-24 21:24:56 +0100

    added xmlhttp request

--- tracker.js
+++ tracker.js
@@ -1,36 +1,24 @@
-function apiRequest(link) {
-  console.log(link);
+function apiPostRequest(e, url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  let data = JSON.stringify({
+    ln: e.target.href,
+    pn: e.target.innerText
+  });
+  xhr.send(data);
+  console.log(data)
 }
 
 let navElements = document.getElementsByClassName("primary-nav__list-item-link");
 
 for (var i = 0; i < navElements.length; i++) {
-
   navElements[i].addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(event.target.href)
-    console.log(event.target.innerText)
+    console.log(event.target.href);
+    console.log(event.target.innerText);
 
-    // apiRequest("hello");
+    apiPostRequest(event, "www.tesco.com");
   });
 }
 
-//    for (var i = 0; i < Anchors.length ; i++) {
-//     Anchors[i].addEventListener("click",
-//         function (event) {
-//             event.preventDefault();
-//             if (confirm('Are you sure?')) {
-//                 window.location = this.href;
-//             }
-//         },
-//         false);
-// }
-
-// var trackInternalLink = function(url) {
-//     gtag('event', 'click', {
-//       'event_category': 'internal urls',
-//       'event_label': url,
-//       'transport_type': 'beacon',
-//       'event_callback': function(){document.location = url;}
-//     });
-//   }
