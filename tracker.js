0afa638c0e65a8bf4c96b696b595722d9c8b5c3a
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-10-27 14:20:13 +0100

    clicking links now updates hash table

--- tracker.js
+++ tracker.js
@@ -1,3 +1,4 @@
+let counter = {}
 function apiPostRequest(e, url) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", url, true);
@@ -7,10 +8,21 @@ function apiPostRequest(e, url) {
     pn: e.target.innerText
   });
   xhr.send(data);
-  console.log(data)
+  console.log(data);
+}
+function updateCounter(link) {
+  counter[link] == null ? (counter[link] = 1) : (counter[link] += 1);
+    console.log(counter);
+  }
+
+
+function writeToCookie(link) {
+  console.log(link);
 }
 
-let navElements = document.getElementsByClassName("primary-nav__list-item-link");
+let navElements = document.getElementsByClassName(
+  "primary-nav__list-item-link"
+);
 
 for (var i = 0; i < navElements.length; i++) {
   navElements[i].addEventListener("click", function(event) {
@@ -19,6 +31,28 @@ for (var i = 0; i < navElements.length; i++) {
     console.log(event.target.innerText);
 
     apiPostRequest(event, "www.tesco.com");
+    updateCounter(event.target.innerText);
   });
 }
 
+// var navElements = document.getElementsByClassName(“nav-item__link”);
+
+// for (var i = 0; i < navElements.length; i++) {
+
+//  navElements[i].addEventListener(“click”, apiRequest);
+
+// }
+
+// function apiRequest(){
+//  var params = `ln=${event.target.href}&pn=${event.target.innerText}`
+//  var url = ‘https:/tesco.com’
+//  var request = new XMLhttpRequest();
+//  request.open(‘POST’, url, true)
+//  request.setRequestHeader(“Content-type”, “application/x-www-form-urlencoded”);
+//  request.send(params);
+//  alert(‘hello’)
+// }
+
+// function setSessionCookie(){
+//  document.cookie =“counter=” + 1 + “;pn=” + event.target.innerText + “;ln=” + event.target.href + “;max-age=” + (60 * 60 * 24 *30) + “;”
+// }
